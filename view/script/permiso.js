b6d6eee8ebada139f6811453a6848bcbbfc023bc
Merlin672098 <alvarrxd@hotmail.com>
2023-08-08 21:22:23 -0400

    Se agrega el proyecto

---
+++ view/script/permiso.js
@@ -0,0 +1,42 @@
+var tabla;
+
+//Función que se ejecuta al inicio
+function init(){
+	listar();
+	mostrarform(false);
+}
+
+//Función limpiar
+function limpiar()
+{
+	$("#nombrepermiso").val("");
+}
+//Función Listar
+function listar()
+{
+	tabla=$('#tbllistado').dataTable(
+	{
+		"aProcessing": true,//Activamos el procesamiento del datatables
+	    "aServerSide": true,//Paginación y filtrado realizados por el servidor
+	    dom: 'Bfrtip',//Definimos los elementos del control de tabla
+	    buttons: [		          
+		            'copyHtml5',
+		            'excelHtml5',
+		            'csvHtml5',
+		            'pdf'
+		        ],
+		"ajax":
+				{
+					url: '../ajax/permiso.php?op=0',
+					type : "get",
+					dataType : "json",						
+					error: function(e){
+						console.log(e.responseText);	
+					}
+				},
+		"bDestroy": true,
+		"iDisplayLength": 10,//Paginación
+	    "order": [[ 0, "desc" ]]//Ordenar (columna,orden)
+	}).DataTable();
+}
+init();
